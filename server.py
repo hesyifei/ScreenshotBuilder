988d5b454b4516fdc705624c744cec70c7f9572b
Jason Ho <eflyjason@gmail.com>
2016-07-20 10:45:11 +0800

    add basic server code

---
+++ server.py
@@ -0,0 +1,35 @@
+import os, tempfile
+from flask import Flask, request, render_template, send_from_directory, url_for, jsonify
+from werkzeug import secure_filename
+
+app = Flask(__name__)
+app.config['ALLOWED_EXTENSIONS'] = set(['jpg', 'jpeg', 'png'])
+
+def allowed_file(filename):
+	return '.' in filename and \
+			filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route("/")
+def homePage():
+	return render_template('index.html')
+
+@app.route('/generateImage', methods=['POST'])
+def upldfile():
+	if request.method == 'POST':
+		print(request.form['inputText'])
+		files = request.files['inputImage']
+		print(files)
+		if files and allowed_file(files.filename):
+			with tempfile.TemporaryDirectory() as temp_dir:
+				filename = secure_filename(files.filename)
+				print(filename)
+				print(temp_dir)
+				files.save(os.path.join(temp_dir, filename))
+				file_size = os.path.getsize(os.path.join(temp_dir, filename))
+				print(file_size)
+				return jsonify(name=filename, size=file_size)
+
+
+if __name__ == "__main__":
+	app.run()
+
---
+++ static/scripts/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+	console.log("document ready");
+
+
+	// Clear event
+	$('.image-preview-clear').click(function(){
+		$('.image-preview-filename').val("");
+		$('.image-preview-clear').hide();
+		$('.image-preview-input input:file').val("");
+		$(".image-preview-input-title").text("Browse");
+	});
+	// Create the preview image
+	$(".image-preview-input input:file").change(function (){
+		var file = this.files[0];
+		var reader = new FileReader();
+		// Set preview image into the popover data-content
+		reader.onload = function (e) {
+			$(".image-preview-input-title").text("Change");
+			$(".image-preview-clear").show();
+			$(".image-preview-filename").val(file.name);
+		}
+		reader.readAsDataURL(file);
+	});
+
+
+	// NOTE: #mainForm is form ID
+	$("#mainForm").submit(function(event){
+
+		console.log("submitted #main-form form");
+		event.preventDefault();
+
+		$inputText = $("#inputText");
+		$inputImage = $("#inputImage");
+		if( (!$.trim($inputText.val())) || (!$.trim($inputImage.val())) ){
+			alert("Input cannot be empty!");
+		}else{
+			var serializedData = new FormData($('#mainForm')[0]);
+
+			request = $.ajax({
+				type: $(this).attr('method') || 'POST',
+				url: $(this).attr('action') || window.location.pathname + window.location.search,
+				data: serializedData,
+				contentType: false,
+				processData: false
+			});
+
+			request.done(function (response, textStatus, jqXHR){
+				console.log("AJAX get success result: "+response);
+
+				//$("p#send-result").html(response);
+
+			});
+
+			request.fail(function (jqXHR, textStatus, errorThrown){
+				console.error("AJAX get error: "+textStatus, errorThrown);
+			});
+
+			request.always(function () {
+			});
+		}
+	});
+
+});
